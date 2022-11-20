29138d1e1cab4fe5c7bfcf308f015f6e1035c991
witspirit <witspirit@gmail.com>
2022-11-20 16:22:57 +0000

    Skeleton Spring Boot setup from initializer.

---
+++ server/src/main/java/be/witspirit/flashcards/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package be.witspirit.flashcards.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
